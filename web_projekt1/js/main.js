2f4c9bec203f01f36c7ffd8aab9a615e8024b280
Tóth Ádám <dwedicxofficial@icloud.com>
2023-09-25 10:07:40 +0200

    d

--- web_projekt1/js/main.js
+++ web_projekt1/js/main.js
@@ -2,6 +2,7 @@ let sorok = 3
 let cellak = 3
 let tt = new Array(sorok)
 let ujSor
+let ures = sorok * cellak - 1;
 
 function loadPuzzle() {
     for (let i = 0; i < sorok; i++) {
@@ -14,14 +15,14 @@ function loadPuzzle() {
             tt[i][j].onclick = function() {lepes(this)}
             tt[i][j].onmouseover = function() {hover(this)}
             tt[i][j].onmouseleave = function() {hoverLeave(this)}
-            
+
             tt[i][j].style.border = "darkgray 2px solid"
             tt[i][j].style.background = "lightgray"
+            tt[i][j].style.width = "33%"
             tt[i][j].innerHTML = parseInt(tt[i][j].id) + 1
         }
     }
 
-    let ures = sorok * cellak - 1;
 	document.getElementById(ures.toString()).style.background="gray";
 	document.getElementById(ures.toString()).innerHTML="";
 }
@@ -41,8 +42,40 @@ function hoverLeave(doc) {
 }
 
 function kever() {
-    let xlepes
-    let ylepes
+    for (let i = 0; i < sorok * cellak * 6; i++) {
+      sorlepes = Math.floor(ures / cellak)
+      cellalepes = ures % cellak
+      
+      switch (Math.floor(Math.random() * 4)) {
+          case 0: // jobbra vagy balra
+            cellalepes += (cellalepes < cellak - 1) ? 1 : -1
+              break
+          case 1: // balra vagy jobbra
+            cellalepes += (cellalepes > 0) ? -1 : 1
+              break
+          case 2: // le vagy fel
+            sorlepes += (sorlepes < sorok -1) ? 1 : -1
+              break
+          case 3: // fel vagy le
+            sorlepes += (sorlepes > 0) ? -1 : 1
+              break
+      }
+      lepes(document.getElementById((sorlepes * cellak + cellalepes).toString()))
+    }
+}
 
-
+function lepes(doc) {
+    var nid = parseInt(doc.id)
+    var nures = parseInt(ures)
+         
+    if (nid + sorok == nures || nid - sorok == nures ||
+        nid+1==nures && nures % sorok != 0 ||
+        nid-1==nures && nid % sorok != 0) {
+        
+        document.getElementById(ures.toString()).innerHTML = document.getElementById(doc.id).innerHTML
+        document.getElementById(ures.toString()).style.background = document.getElementById(doc.id).style.background
+        document.getElementById(doc.id).style.background="gray"
+        document.getElementById(doc.id).innerHTML=""
+        ures=doc.id;
+    }
 }
